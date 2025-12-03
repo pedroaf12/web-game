ec9aa2484b258af4e72051d69ee8a9e1f011f9f5
pedroaf12 <51967841+pedroaf12@users.noreply.github.com>
2025-12-03 09:43:58 -0300

    melhorando jogabilidade
    Foi adicionado um placar para marcar os pontos por acertos
    E um botão para reiniciar quando o o jogador perder

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const reset = document.querySelector('.reset')
+
+let points = 0;
+let canCount = true;
 
 const jump = () => {
     mario.classList.add('jump');
@@ -8,12 +12,34 @@ const jump = () => {
         mario.classList.remove('jump')
     }, 500);
 }
+function addPoints() {
+    points += 1;
+    attScore();
+}
+
+function attScore() {
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent =  "Pontos: " + points
+}
+
+function characterDie() {
+            const buttomReset = document.getElementById("reset")
+            buttomReset.style.display = "block"
+        }
 
 const loop = setInterval(() =>{
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
+    if (pipePosition < 0 && canCount) {
+        addPoints();
+        canCount = false;
+    }
+
+    if (pipePosition > 120) {
+        canCount = true;
+    }
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ) {
 
@@ -26,11 +52,22 @@ const loop = setInterval(() =>{
         mario.src ="images/mario-jump-images/game-over.png";
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
+
+        characterDie()
         
         clearInterval(loop)
     }
 
 }, 10)
+
+
+
+function reiniciarGame() {
+
+    location.reload();
+}
+
+
 
 document.addEventListener("keydown", jump)
 
